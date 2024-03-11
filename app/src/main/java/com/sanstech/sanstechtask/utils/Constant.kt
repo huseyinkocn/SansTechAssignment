0695b0cce36b2f91e5c3d9fc31219ecfb066fff8
huseyinkoc1 <huseyin.koc1@ogr.altinbas.edu.tr>
2024-03-11 11:43:42 +0300

    added base_url

---
+++ app/src/main/java/com/sanstech/sanstechtask/utils/Constant.kt
@@ -0,0 +1,9 @@
+package com.sanstech.sanstechtask.utils
+
+class Constant {
+
+    companion object {
+        const val BASE_URL ="https://apivx.misli.com/api/"
+        //https://apivx.misli.com/api/mobile/v2/statistics/sport/SOCCER/matches
+    }
+}
